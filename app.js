525183852dbab253f49e4e8443fe9b3b39276565
tji132006 <tji132006@gmail.com>
2018-07-24 15:49:32 -0700

    style the campgrounds and new ejs pages with navbar and form using bootstrap

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 var campgrounds = [
 		{name: "Mt. San Juan", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRHI0jF1PAZ3q9wCCTWgPqNdooNG5pCZj2Se9L8rWO-ClUdp2vw"},
 		{name: "Louis River", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqh_-b9CLK0JLEpu3dKpf0ZlbVZwiGw_CSZk4-cH3p0ZZ3e38j0Q"},
+		{name: "Turto Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQar4ImG82-rEnz7NXQd8hpru9QQ-psAazFMC1fRtLs1ypfZrc"},
+		{name: "Mt. San Juan", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRHI0jF1PAZ3q9wCCTWgPqNdooNG5pCZj2Se9L8rWO-ClUdp2vw"},
+		{name: "Louis River", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqh_-b9CLK0JLEpu3dKpf0ZlbVZwiGw_CSZk4-cH3p0ZZ3e38j0Q"},
+		{name: "Turto Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQar4ImG82-rEnz7NXQd8hpru9QQ-psAazFMC1fRtLs1ypfZrc"},
+		{name: "Mt. San Juan", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRHI0jF1PAZ3q9wCCTWgPqNdooNG5pCZj2Se9L8rWO-ClUdp2vw"},
+		{name: "Louis River", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqh_-b9CLK0JLEpu3dKpf0ZlbVZwiGw_CSZk4-cH3p0ZZ3e38j0Q"},
 		{name: "Turto Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQar4ImG82-rEnz7NXQd8hpru9QQ-psAazFMC1fRtLs1ypfZrc"}
+
+
 ]
 
 //landing page
